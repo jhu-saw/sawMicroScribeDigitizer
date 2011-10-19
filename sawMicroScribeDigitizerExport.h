ea9399f3f95555243cfe38df7bb936fda6ec5cf4
Min Yang Jung <myj@jhu.edu>
2011-10-19 22:02:48 +0000

    Added saw wrapper for the MicroScribe digitizer with two example applications (console-based one and GUI(FLTK)-based one)
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk/saw/components/sawMicroScribeDigitizer@3080 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ sawMicroScribeDigitizerExport.h
@@ -0,0 +1,28 @@
+/*
+  $Id: sawMicroScribeDigitizerExport.h 2973 2011-09-27 17:30:43Z auneri1 $
+
+  Author(s):  Min Yang Jung
+  Created on: 2011-10-06
+
+  (C) Copyright 2011 Johns Hopkins University (JHU), All Rights Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+// check if this module is build as a DLL
+#ifdef sawMicroScribeDigitizer_EXPORTS
+#define CISST_THIS_LIBRARY_AS_DLL
+#endif
+
+// include common defines
+#include <cisstCommon/cmnExportMacros.h>
+
+// avoid impact on other modules
+#undef CISST_THIS_LIBRARY_AS_DLL
+
